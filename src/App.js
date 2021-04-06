0b8e1dec5ceed44b0b15208e40870fbda7700f07
pkyy <ai9gle@gmail.com>
2021-04-06 20:50:27 +0900

    hooks end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Average from './Average'
+import Info from './Info'
 
 function App() {
    
   return (
     <div>
-      <Average />
+      <Info />
     </div>
   );
 }
--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useCallback, useRef } from 'react';
 
 const getAverage = numbers => {
     console.log('평균값 계산 중...');
@@ -10,27 +10,31 @@ const getAverage = numbers => {
 const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
+    const inputEl = useRef(null);
 
-    const onChange = e => {
+    const onChange = useCallback(e => {
         setNumber(e.target.value);
-    };
-    const onInsert = e => {
+    }, []);
+    const onInsert = useCallback(e => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    };
+        inputEl.current.focus(); 
+    }, [number, list]);
+
+    const avg = useMemo(() => getAverage(list), [list]);
 
     return (
         <div>
-            <input value={number} onChange={onChange} />
+            <input value={number} onChange={onChange} ref={inputEl}/>
             <button onClick={onInsert}>등록</button>
-            <ui>
+            <ul>
                 {list.map((value, index) => (
                     <li key={index}>value</li>
                 ))}
-            </ui>
+            </ul>
             <div>
-                <b>평균값:</b> {getAverage(list)}
+                <b>평균값:</b> {avg}
             </div>
         </div>
     )
